9af09ab4f02a7a13ca07bae997368a972d5cc088
fuji4046 <montefuscoangela06@gmail.com>
2024-05-16 16:53:41 +0200

    Section finito

--- res/js/script_sec.js
+++ res/js/script_sec.js
@@ -1,42 +1,29 @@
-function setActivebubble() {
+function setActiveBubble() {
     var windowHeight = window.innerHeight;
     var scrollTop = window.scrollY;
-    var middle = (scrollTop + windowHeight / 2);
+    var middle = scrollTop + windowHeight / 2;
 
-    // Trova il blocco più vicino al centro dello schermo
-    var closestbubble = null;
+    var closestBubble = null;
     var minDistance = Infinity;
 
-    // Seleziona tutti gli elementi con classe 'bubble'
     var bubbles = document.querySelectorAll('.bubble');
-    // Itera su ciascun blocco
     bubbles.forEach(function(bubble) {
-        // Ottieni le dimensioni e la posizione del blocco rispetto alla finestra visualizzata
         var bubbleRect = bubble.getBoundingClientRect();
-        // Calcola il punto medio verticale del blocco
         var bubbleMiddle = bubbleRect.top + scrollTop + bubbleRect.height / 2;
-        // Calcola la distanza assoluta tra il punto medio del blocco e il punto medio dello schermo
         var distanceToMiddle = Math.abs(middle - bubbleMiddle);
 
-        // Se la distanza al centro dello schermo è minore della distanza minima trovata finora
         if (distanceToMiddle < minDistance) {
-            // Aggiorna la distanza minima e il blocco più vicino al centro
             minDistance = distanceToMiddle;
-            closestbubble = bubble;
+            closestBubble = bubble;
         }
     });
 
-    // Rimuovi la classe active da tutti i blocchi
     bubbles.forEach(function(bubble) {
         bubble.classList.remove('active');
     });
 
-    // Aggiungi la classe active al blocco più vicino al centro
-    closestbubble.classList.add('active');
+    closestBubble.classList.add('active');
 }
 
-// Aggiungi un ascoltatore per l'evento scroll per aggiornare il blocco attivo durante lo scrolling
-window.addEventListener('scroll', setActivebubble);
-
-// Chiama la funzione per impostare il blocco attivo quando la pagina viene caricata
-window.addEventListener('load', setActivebubble);
+window.addEventListener('scroll', setActiveBubble);
+window.addEventListener('load', setActiveBubble);
