051b878a98d17f0dd3ce8c0a3d7555ae21eb795e
Ghibbi64 <marioghibbi2@gmail.com>
2024-05-18 23:04:44 +0200

    test 2 sfondo

--- res/js/base.js
+++ res/js/base.js
@@ -5,7 +5,7 @@ var direction = 'w';
 function bgscroll() {
     current -= 1;
     $('body').css("backgroundPosition", (direction == 'h') ? current + "px 0" : "0 " + current + "px");
-
 }
-setInterval("bgscroll()", scrollSpeed);
-//----------------------------------------------------
+window.onload = function() {
+    setInterval("bgscroll()", scrollSpeed);
+};
