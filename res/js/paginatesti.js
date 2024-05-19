c26e84ef47daa58325b34b76064910bd15b46743
gr3vX <danielemanno@istitutoangioy.edu.it>
2024-05-19 14:05:41 +0200

    .

---
+++ res/js/paginatesti.js
@@ -0,0 +1,19 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction(id) {
+  document.getElementById(id).classList.toggle("showclick");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+if (!event.target.matches('.dropbtnclick')) {
+  var dropdowns = document.getElementsByClassName("dropdown-content-click");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('showclick')) {
+      openDropdown.classList.remove('showclick');
+    }
+  }
+}
+}
