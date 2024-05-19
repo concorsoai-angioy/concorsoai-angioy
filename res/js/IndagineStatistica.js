8920d81b54779d241a41d83e9f9fd97fc18197c2
fuji4046 <montefuscoangela06@gmail.com>
2024-05-19 11:21:56 +0200

    Animazioni IndagineStatistica2 cambiate

--- res/js/IndagineStatistica.js
+++ res/js/IndagineStatistica.js
@@ -1,13 +1,20 @@
+inclus
+gsap.registerPlugin(ScrollTrigger);
 let tl = gsap.timeline({
-    scrolltrigger: {
-        trigger: '.container',
+    scrollTrigger: {
+        trigger: '.block',
+        //start: '20% center',
         start: 'top center',
         end: 'bottom center',
+        //end: 'bottom 60%',
         scrub: true,
         markers: true
     }
 });
 
-tl.to('.container', {
-    opacity: 1
+tl.to('.block', {
+    opacity: 1,
+    duration: 2,
+    pinSpacing: true, // se div padre è flex
+    scale: 1
 });
