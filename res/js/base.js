1cdb084c6342d9eebafcc9abf58c4429ddf97c4b
Ghibbi64 <marioghibbi2@gmail.com>
2024-05-20 11:20:44 +0200

    dark mode palette

--- res/js/base.js
+++ res/js/base.js
@@ -9,3 +9,25 @@ function bgscroll() {
 window.onload = function() {
     setInterval("bgscroll()", scrollSpeed);
 };
+
+function darkmode(flag) {
+
+    if(flag){
+        var palette = ["#E6E6E6", "#000", "#E6E6E6", "#001D4A", "#0084e7", "#E6E6E6", "#FFB60A", "#FFB60A", "#040549", "#060f24"];
+    }else{
+        var palette = ["#E6E6E6", "#000", "#E6E6E6", "#001D4A", "#0084e7", "#E6E6E6", "#FFB60A", "#FFB60A", "#040549", "#060f24"];
+    }
+
+    document.documentElement.style.setProperty('--testo', '#FFB60A');
+    document.documentElement.style.setProperty('--testo2', '#0084e7');
+    document.documentElement.style.setProperty('--sfondo', '#001D4A');
+    document.documentElement.style.setProperty('--sfondoDiv', '#E6E6E6');
+    document.documentElement.style.setProperty('--sfondoDiv2', '#000');
+    document.documentElement.style.setProperty('--titoli', '#FFB60A');
+    document.documentElement.style.setProperty('--dettagli', '#E6E6E6');
+    document.documentElement.style.setProperty('--backbutton', '#E6E6E6');
+    document.documentElement.style.setProperty('--sfondonav', '#060f24');
+    document.documentElement.style.setProperty('--sfondonav2', '#040549');
+}
+
+// Esegui la funzione quando necessar
