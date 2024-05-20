5a22ef1d1c1bba7cda1250534f97db0279086751
gr3vX <danielemanno@istitutoangioy.edu.it>
2024-05-20 22:56:27 +0200

    ,

--- res/js/paginatesti.js
+++ res/js/paginatesti.js
@@ -18,3 +18,28 @@ window.onclick = function (event) {
   }
 }
 
+gsap.registerPlugin(ScrollTrigger);
+
+let tx = gsap.timeline({
+  scrollTrigger: {
+      trigger: '.col1',
+      start: 'top 0%',
+      end: 'bottom 2%',
+      scrub: true,
+      markers: true
+  }
+});
+
+tx.fromTo('header', {
+  width: '100%',
+  marginInline: '0%',
+  borderRadius: '0px',
+  position: 'none',
+}, {
+  width: '80%',
+  marginInline: '10%',
+  borderRadius: '300px',
+  duration: 10,
+  position: 'fixed',
+  marginTop: '2%',
+});
