6063910965c2788af722d9f5abaf93578967abc6
fuji4046 <montefuscoangela06@gmail.com>
2024-05-20 23:24:34 +0200

    aaa

--- res/js/paginatesti.js
+++ res/js/paginatesti.js
@@ -17,29 +17,3 @@ window.onclick = function (event) {
     }
   }
 }
-
-gsap.registerPlugin(ScrollTrigger);
-
-let tz = gsap.timeline({
-  scrollTrigger: {
-    trigger: 'col1',
-    start: 'top 0%',
-    end: 'bottom 2%',
-    scrub: true,
-    markers: true
-  }
-});
-
-tz.fromTo('col1', {
-  width: '100%',
-  marginInline: '0%',
-  borderRadius: '0px',
-  position: 'none',
-}, {
-  width: '80%',
-  marginInline: '10%',
-  borderRadius: '300px',
-  duration: 10,
-  position: 'fixed',
-  marginTop: '2%',
-});
