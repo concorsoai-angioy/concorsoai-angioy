82ace74f55b6111afe769151eded05b9bd99b226
fuji4046 <montefuscoangela06@gmail.com>
2024-05-20 21:50:12 +0200

    é diventano è, navbar inizia a muoversi

---
+++ res/js/navbar.js
@@ -0,0 +1,25 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let tx = gsap.timeline({
+    scrollTrigger: {
+        trigger: 'nav',
+        start: 'top 0%',
+        end: 'bottom 2%',
+        scrub: true,
+        markers: false
+    }
+});
+
+tx.fromTo('header', {
+    width: '100%',
+    marginInline: '0%',
+    borderRadius: '0px',
+    position: 'none'
+}, {
+    width: '80%',
+    marginInline: '10%',
+    borderRadius: '300px',
+    duration: 10,
+    position: 'fixed',
+    marginTop: '2%'
+});
