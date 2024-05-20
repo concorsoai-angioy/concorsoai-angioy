118689715147a9c4a12fa43295c73ce54f80b621
Ghibbi64 <marioghibbi2@gmail.com>
2024-05-20 12:28:32 +0200

    dark mode migliroata

--- res/js/base.js
+++ res/js/base.js
@@ -10,24 +10,36 @@ window.onload = function() {
     setInterval("bgscroll()", scrollSpeed);
 };
 
-function darkmode(flag) {
-
-    if(flag){
-        var palette = ["#E6E6E6", "#000", "#E6E6E6", "#001D4A", "#0084e7", "#E6E6E6", "#FFB60A", "#FFB60A", "#040549", "#060f24"];
-    }else{
-        var palette = ["#E6E6E6", "#000", "#E6E6E6", "#001D4A", "#0084e7", "#E6E6E6", "#FFB60A", "#FFB60A", "#040549", "#060f24"];
-    }
-
-    document.documentElement.style.setProperty('--testo', '#FFB60A');
-    document.documentElement.style.setProperty('--testo2', '#0084e7');
-    document.documentElement.style.setProperty('--sfondo', '#001D4A');
-    document.documentElement.style.setProperty('--sfondoDiv', '#E6E6E6');
-    document.documentElement.style.setProperty('--sfondoDiv2', '#000');
-    document.documentElement.style.setProperty('--titoli', '#FFB60A');
-    document.documentElement.style.setProperty('--dettagli', '#E6E6E6');
-    document.documentElement.style.setProperty('--backbutton', '#E6E6E6');
-    document.documentElement.style.setProperty('--sfondonav', '#060f24');
-    document.documentElement.style.setProperty('--sfondonav2', '#040549');
+if(sessionStorage.getItem("isDark") == null){
+    var isDark = false;
+}else{
+    var isDark = session.getItem("isDark");
 }
 
-// Esegui la funzione quando necessar
+function isDarkMode(){
+    return sessionStorage.getItem("isDark");
+}
+
+function darkmode(){
+    if(isDarkMode()){
+        var palette = ["#E6E6E6", "#E6E6E6", "#1B262C",  "#001D4A",  "#1c6495",  "#1B262C",  "#E6E6E6",  "#E6E6E6",  "#FFB60A",  "#FFB60A", "#040549", "#060f24"];
+    }else{
+        var palette = ["#E6E6E6", "#000", "#E6E6E6",  "#001D4A",  "#0084e7",  "#E6E6E6",  "#0084e7",  "#E6E6E6",  "#FFB60A", "#FFB60A", "#040549", "#060f24"];
+    }
+
+    // console.log(isDarkMode())
+    // sessionStorage.setItem("isDark", !sessionStorage.getItem("isDark"));
+
+    document.documentElement.style.setProperty('--testo', palette[0]);
+    document.documentElement.style.setProperty('--testo2', palette[1]);
+    document.documentElement.style.setProperty('--sfondo', palette[2]);
+    document.documentElement.style.setProperty('--sfondoDiv', palette[3]);
+    document.documentElement.style.setProperty('--sfondoDiv2', palette[4]);
+    document.documentElement.style.setProperty('--sfondoTestoFooter', palette[5]);
+    document.documentElement.style.setProperty('--testoFooter', palette[6]);
+    document.documentElement.style.setProperty('--titoli', palette[7]);
+    document.documentElement.style.setProperty('--dettagli', palette[8]);
+    document.documentElement.style.setProperty('--backbutton', palette[9]);
+    document.documentElement.style.setProperty('--sfondonav', palette[10]);
+    document.documentElement.style.setProperty('--sfondonav2', palette[11]);
+}
